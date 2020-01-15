6ad0f7929f53dede6f861fe8a67a0fbc50b69d37
navarroruben <rubenxnavarro@gmail.com>
2020-01-15 12:45:05 -0500

    Code drastically reduced to provide greater time and space complexity.

--- definitions.cpp
+++ definitions.cpp
@@ -21,56 +21,23 @@ void displayMatrix(std::vector<std::vector<int> > v) {
 void locate2DZero(std::vector<std::vector<int> > &v) {
     // vector of pairs to hold x and y coordinates of 0s in matrix
     std::vector<std::pair<int, int>> p;
-    bool flag = false;
     // check for matrix and assign rows and col to 0
-     if (v.size() > 1 && v[0].size() > 1) {
-         for (int row = 0; row < v.size(); ++row) {
-             for (int col = 0; col < v[0].size(); ++col) {
-                 if (v[row][col] == 0) {
-                     p.push_back(std::make_pair(row, col));
-                 }
-             }
-         }
-         // gets data from container to assign 0s to rows and columns
-         for (int i = 0; i < p.size(); ++i) {
-             // assign 0s to rows
-             for (int col = 0; col < v[p[i].first].size(); ++col) {
-                 v[p[i].first][col] = 0;
-             }
-             // assign 0s to columns
-             for (int row = 0; row < v.size(); ++row) {
-                 v[row][p[i].second] = 0;
-             }
-         }
-     }
-    // if there is only data in the x axis, sets column to 0 if 0 is found
-    else if (v.size() > 1 && v[0].size() == 1) {
-        for (int row = 0; row < v.size();) {
-        if (flag) {
-            v[row][0] = 0;
+    for (int row = 0; row < v.size(); ++row) {
+        for (int col = 0; col < v[0].size(); ++col) {
+            if (v[row][col] == 0) {
+                p.push_back(std::make_pair(row, col));
+            }
         }
-
-        if (v[row][0] == 0 && !flag) {
-            flag = true;
-            row = 0;
-        }
-        else
-            ++row;
-         }
     }
-    // if there is only data in the y axis, sets column to 0 if 0 is found
-    else if (v.size() == 1 && v[0].size() > 1) {
-
-        for (int col = 0; col < v[0].size();) {
-            if (flag) {
-                v[0][col] = 0;
-            }
-            if (v[0][col] == 0 && !flag) {
-                flag = true;
-                col = 0;
-            }
-            else
-                ++col;
+    // gets data from container to assign 0s to rows and columns
+    for (int i = 0; i < p.size(); ++i) {
+        // assign 0s to rows
+        for (int col = 0; col < v[p[i].first].size(); ++col) {
+            v[p[i].first][col] = 0;
+        }
+        // assign 0s to columns
+        for (int row = 0; row < v.size(); ++row) {
+            v[row][p[i].second] = 0;
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ int main() {
     // test cases
 
     // std::vector<std::vector<int>> v {{0}};
-   // std::vector<std::vector<int>> v {{0, 1, 2, 0}, {3, 1, 5, 2}, {1, 3, 1, 5}};
-      std::vector<std::vector<int>> v {{0, 1, 0}, {1, 1, 1}, {1, 1, 1}};
+    std::vector<std::vector<int>> v {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+   //   std::vector<std::vector<int>> v {{0, 1, 0}, {1, 1, 1}, {1, 1, 1}};
    // std::vector<std::vector<int>> v {{1, 1, 0, 1}};
    // std::vector<std::vector<int>> v {{1},{1}};
    // std::vector<std::vector<int>> v {{1, 1, 1}};
 
-  // locates 0 in 2d array
+  // locates 0 in 2d array4.
    locate2DZero(v);
 
    // displays matrix
